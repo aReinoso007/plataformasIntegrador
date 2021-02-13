41eebfd8fba1c79fda3f68a20a599f54ba8206e4
Alex Reinoso <ralexjessiel@gmail.com>
2021-02-12 19:18:51 -0500

    Terminado registro con el mapa

--- src/app/shared/pages/userInfo/userinfo.module.ts
+++ src/app/shared/pages/userInfo/userinfo.module.ts
@@ -7,13 +7,20 @@ import { IonicModule } from '@ionic/angular';
 import { UserinfoPageRoutingModule } from './userinfo-routing.module';
 
 import { UserinfoPage } from './userinfo';
+import { AgmCoreModule } from '@agm/core';
 
+//c98ee318-8238-429c-aa21-d0cc02f7e9ea
+//AIzaSyCrTJjf1ciS5DS5feqEWFX1pntGc7MFJi4
+//AIzaSyCT9wzsIIAkW95uHWVvCbBEP-xtjNbJPow
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    UserinfoPageRoutingModule
+    UserinfoPageRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyCT9wzsIIAkW95uHWVvCbBEP-xtjNbJPow'
+    })
   ],
   declarations: [UserinfoPage]
 })
